9a4ad219def5f4a2ee127d8ed81e868fd1c4ead5
Ardit Qerosi <arditqerosi@CRSs-iMac-4.local>
2022-06-06 13:26:43 +0200

    added check for null usage

--- src/Models/PlanSubscription.php
+++ src/Models/PlanSubscription.php
@@ -574,6 +574,10 @@ class PlanSubscription extends Model
             return true;
         }
 
+        if (is_null($usage)) {
+            return true;
+        }
+
         // If the feature value is zero, let's return false since
         // there's no uses available. (useful to disable countable features)
         if ($usage->expired() || is_null($featureValue) || $featureValue === '0' || $featureValue === 'false') {
